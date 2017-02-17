560a5cc753ad1195652292c17f519e6883b2f30f
talasila <talasila@research.att.com>
2017-02-17 13:39:09 -0500

    docker repo added
    Change-Id: Iddea458d4ebba8131519cb68d4c0f417d4c09e37
    Signed-off-by: talasila <talasila@research.att.com>

--- deliveries/os_docker_push.sh
+++ deliveries/os_docker_push.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-#docker login
-docker tag ep:1610-1 portalapps:1.0.0
-docker tag ecompdb:portal portaldb:1.0.0
-docker push portalapps:1.0.0
-docker push portaldb:1.0.0
+REPO="nexus3.openecomp.org:10003"
+
+APPS="${REPO}/openecomp/portalapps:1.0.0"
+DB="${REPO}/openecomp/portaldb:1.0.0"
+
+# docker login nexus3.openecomp.org:10003
+
+# tag
+docker tag ep:1610-1 ${APPS}
+docker tag ecompdb:portal ${DB}
+
+# push
+docker push ${APPS}
+docker push ${DB}
