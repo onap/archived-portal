b10edf91b69598626d6d1628d5ab873261118456
talasila <talasila@research.att.com>
2017-02-22 11:02:09 -0500

    release script added
    Change-Id: I837c47edd8c06ba4ebd72b17c6a4f36e9c44e2a5
    Signed-off-by: talasila <talasila@research.att.com>

---
+++ deliveries/os_docker_release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+./run.sh
+
+REPO="nexus3.openecomp.org:10002"
+
+APPS="${REPO}/openecomp/portalapps:1.0.0"
+DB="${REPO}/openecomp/portaldb:1.0.0"
+
+# docker login nexus3.openecomp.org:10002
+
+# tag
+docker tag ep:1610-1 ${APPS}
+docker tag ecompdb:portal ${DB}
+
+# push
+docker push ${APPS}
+docker push ${DB}
