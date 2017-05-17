ba838f2e13f1e8050c75e68bd3733d56d8f416d5
statta <statta@research.att.com>
2017-05-17 08:08:49 -0400

    [Docker Build] Updated Docker build
    The changes have been performed in the build script to align with
    Rebased code
    
    
    Change-Id: Idedd7d9b10124af2a523349fc8d9744e8ea06fd8
    Signed-off-by: statta <statta@research.att.com>

--- deliveries/createMaria.sh
+++ deliveries/createMaria.sh
@@ -4,4 +4,4 @@ source $(dirname $0)/os_settings.sh
 echo ${SCRIPT_DIR}
 
 
-docker build -t ecompdb:portal --build-arg SCRIPT_DIR="${SCRIPT_DIR}" --build-arg SDK_SCRIPT_DIR="${SDK_SCRIPT_DIR}" --build-arg DBC_SCRIPT_DIR="${DBC_SCRIPT_DIR}" -f ./Dockerfile.mariadb .
+docker build -t ecompdb:portal --build-arg SCRIPT_DIR="${SCRIPT_DIR}" --build-arg SDK_SCRIPT_DIR="${SDK_SCRIPT_DIR}" --build-arg DBC_SCRIPT_DIR="${DBC_SCRIPT_DIR}" --build-arg SCRIPT_COMMON_DIR="${SCRIPT_COMMON_DIR}" --build-arg SDK_COMMON_SCRIPT_DIR="${SDK_COMMON_SCRIPT_DIR}" --build-arg DBC_COMMON_SCRIPT_DIR="${DBC_COMMON_SCRIPT_DIR}" -f ./Dockerfile.mariadb .
