de8b8df79c1f0f56d978046a6f5afda05ec72fc4
Christopher Lott (cl778h) <clott@research.att.com>
2017-09-28 16:59:17 -0400

    Update license in non-Java files.
    Add logs and properties directory names to .env file.
    Revise portal_vm_init.sh script to pull and tag onap/cli docker image.
    
    Issue: PORTAL-90
    Change-Id: I82a371ee9797b3e037ad282538cb48b5620c9709
    Signed-off-by: Christopher Lott (cl778h) <clott@research.att.com>

--- deliveries/portal_vm_init.sh
+++ deliveries/portal_vm_init.sh
@@ -12,19 +12,24 @@ NEXUS_USERNAME=$(cat /opt/config/nexus_username.txt)
 NEXUS_PASSWD=$(cat /opt/config/nexus_password.txt)
 NEXUS_DOCKER_REPO=$(cat /opt/config/nexus_docker_repo.txt)
 DOCKER_IMAGE_VERSION=$(cat /opt/config/docker_version.txt)
+# Can use this version instead to use snapshot versions:
+# DOCKER_IMAGE_VERSION=latest
+# CLI has a different version than Portal
+CLI_IMAGE_VERSION=1.1-STAGING-latest
 
 # Refresh configuration and scripts
 cd /opt/portal
 git pull
 cd deliveries
 
-# Get image names used below from docker-compose environment file
+# Get variables from docker-compose environment file
 source .env
 
-# Copy property files
-ETC=/PROJECT/OpenSource/UbuntuEP/etc
-mkdir -p $ETC
-cp -r properties_rackspace/* $ETC
+# Copy property files to new directory
+mkdir -p $PROPS_DIR
+cp -r properties_rackspace/* $PROPS_DIR
+# Also create logs directory
+mkdir -p $LOGS_DIR
 
 # Refresh images
 docker login -u $NEXUS_USERNAME -p $NEXUS_PASSWD $NEXUS_DOCKER_REPO
@@ -32,10 +37,16 @@ docker pull $NEXUS_DOCKER_REPO/openecomp/${DB_IMG_NAME}:$DOCKER_IMAGE_VERSION
 docker pull $NEXUS_DOCKER_REPO/openecomp/${EP_IMG_NAME}:$DOCKER_IMAGE_VERSION
 docker pull $NEXUS_DOCKER_REPO/openecomp/${WMS_IMG_NAME}:$DOCKER_IMAGE_VERSION
 
+# CLI is not built locally
+docker pull $NEXUS_DOCKER_REPO/onap/cli:${CLI_IMAGE_VERSION}
+
 # Tag them as expected by docker-compose file
 docker tag $NEXUS_DOCKER_REPO/openecomp/${DB_IMG_NAME}:$DOCKER_IMAGE_VERSION $DB_IMG_NAME:$PORTAL_TAG
 docker tag $NEXUS_DOCKER_REPO/openecomp/${EP_IMG_NAME}:$DOCKER_IMAGE_VERSION $EP_IMG_NAME:$PORTAL_TAG
 docker tag $NEXUS_DOCKER_REPO/openecomp/${WMS_IMG_NAME}:$DOCKER_IMAGE_VERSION $WMS_IMG_NAME:$PORTAL_TAG
+
+# Tag CLI also
+docker tag $NEXUS_DOCKER_REPO/onap/cli:${CLI_IMAGE_VERSION} onap/cli:$PORTAL_TAG
 
 # compose is not in /usr/bin
 /opt/docker/docker-compose down
