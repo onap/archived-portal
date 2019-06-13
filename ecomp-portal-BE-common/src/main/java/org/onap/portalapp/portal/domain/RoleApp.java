4750a197efd18dd20d8f99e8cb7912ea6fd18587
Dominik Mizyn <d.mizyn@samsung.com>
2019-06-13 17:03:08 +0200

    RoleApp class DB constraints
    Java Bean Validation SR 380 annotations added to classes
    Lombod added to widget-ms
    Plains getter/setter converted to lombok annotation
    
    Issue-ID: PORTAL-629
    Change-Id: I31639672510994412149ed8be92cb8e1b022f646
    Signed-off-by: Dominik Mizyn <d.mizyn@samsung.com>

--- ecomp-portal-BE-common/src/main/java/org/onap/portalapp/portal/domain/RoleApp.java
+++ ecomp-portal-BE-common/src/main/java/org/onap/portalapp/portal/domain/RoleApp.java
@@ -39,90 +39,21 @@ package org.onap.portalapp.portal.domain;
 
 import java.io.Serializable;
 import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-//@Entity
-//@Table(name = "FN_ROLE")
+@Getter
+@Setter
 public class RoleApp implements Serializable{
 	private static final long serialVersionUID = 1L;
 
-	//@Id
-	//@Column(name = "ROLE_ID")
-	//@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long roleId;
-	
-	
-	//@Column(name = "ROLE_Name")
+
 	private String roleName;
-	
-	//@ManyToOne(fetch = FetchType.EAGER)
-	//@JoinColumn(name="APP_ID")
+
 	private App app;
-	
-	//@JsonIgnore
-	//@ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, mappedBy="widgetRoles")
+
 	private Set<WidgetCatalog> widgets;
-
-	/*@PreRemove
-	private void removeGroupsFromUsers() {
-	    for (WidgetCatalog w : widgets) {
-	        w.getWidgetRoles().remove(this);
-	    }
-	}*/
-	
-	/*@ManyToOne
-	@JoinColumn(name = "WIDGET_ID", nullable = false)
-	WidgetCatalog widgetCatalog;*/
-
-	//@JsonIgnore
-	//@ManyToMany(mappedBy = "widgetRoles")
-	//@ManyToMany(fetch = FetchType.EAGER, mappedBy = "widgetRoles")
-	//private Set<WidgetCatalog> widgets  = new HashSet<WidgetCatalog>();
-	
-	public Long getRoleId() {
-		return roleId;
-	}
-
-	public void setRoleId(Long roleId) {
-		this.roleId = roleId;
-	}
-
-	public String getRoleName() {
-		return roleName;
-	}
-
-	public void setRoleName(String roleName) {
-		this.roleName = roleName;
-	}
-
-	public App getApp() {
-		return app;
-	}
-
-	public void setApp(App app) {
-		this.app = app;
-	}
-	
-	
-
-	public Set<WidgetCatalog> getWidgets() {
-		return widgets;
-	}
-
-	public void setWidgets(Set<WidgetCatalog> widgets) {
-		this.widgets = widgets;
-	}
 
 	@Override
 	public String toString() {
