941133a42bad6a1d73c63913a950d1e4bc814fde
Dominik Orliński <d.orlinski@samsung.com>
2019-06-17 11:53:38 +0200

    Fix sql injection vulnerability
    Use a variable binding instead of concatenation.
    Change test 'getAppRolesForNonCentralizedPartnerAppTest'.
    
    Issue-ID: OJSI-174
    Signed-off-by: Dominik Orliński <d.orlinski@samsung.com>
    Change-Id: Ia75da49ed582836a47b5fdcddab62fbe02e36e72

--- ecomp-portal-BE-common/src/test/java/org/onap/portalapp/portal/service/UserRolesCommonServiceImplTest.java
+++ ecomp-portal-BE-common/src/test/java/org/onap/portalapp/portal/service/UserRolesCommonServiceImplTest.java
@@ -440,8 +440,10 @@ public class UserRolesCommonServiceImplTest {
 				.thenReturn(epFunctionalMenuQuery2);
 		Mockito.doReturn(mockFunctionalMenuRolesList).when(epFunctionalMenuQuery2).list();
 
-		Mockito.when(session.createQuery("from " + FunctionalMenuItem.class.getName() + " where menuId=" + 10l))
+		Mockito.when(session.createQuery("from :name where menuId=:menuId"))
 				.thenReturn(epFunctionalMenuItemQuery);
+		Mockito.when(epFunctionalMenuItemQuery.setParameter("name",FunctionalMenuItem.class.getName())).thenReturn(epFunctionalMenuItemQuery);
+		Mockito.when(epFunctionalMenuItemQuery.setParameter("menuId",10l)).thenReturn(epFunctionalMenuItemQuery);
 		Mockito.doReturn(mockFunctionalMenuItemList).when(epFunctionalMenuItemQuery).list();
 		List<EcompRole> mockEcompRoleList2 = new ArrayList<>();
 		EcompRole mockUserAppRoles = new EcompRole();
