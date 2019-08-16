f1714713e6abcdf8551b5fa30e6191f4abe0feb0
Dominik Mizyn <d.mizyn@samsung.com>
2019-08-16 11:49:31 +0200

    Portal Spring Boot version Hibernate implementation
    Create classes that represent tables in the original database.
    
    Issue-ID: PORTAL-705
    Change-Id: Ie4a73c18389ec829db1110e92b8d13877d3436ab
    Signed-off-by: Dominik Mizyn <d.mizyn@samsung.com>

---
+++ portal-BE/src/main/java/org/onap/portal/domain/db/ep/EpWidgetPreviewFiles.java
@@ -0,0 +1,86 @@
+/*
+ * ============LICENSE_START==========================================
+ * ONAP Portal
+ * ===================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ===================================================================
+ * Modifications Copyright (c) 2019 Samsung
+ * ===================================================================
+ *
+ * Unless otherwise specified, all software contained herein is licensed
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this software except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Unless otherwise specified, all documentation contained herein is licensed
+ * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
+ * you may not use this documentation except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             https://creativecommons.org/licenses/by/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, documentation
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ============LICENSE_END============================================
+ *
+ *
+ */
+
+package org.onap.portal.domain.db.ep;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/*
+CREATE TABLE `ep_widget_preview_files` (
+        `preview_id` int(11) NOT NULL AUTO_INCREMENT,
+        `html_file` longblob DEFAULT NULL,
+        `css_file` longblob DEFAULT NULL,
+        `javascript_file` longblob DEFAULT NULL,
+        `framework_file` longblob DEFAULT NULL,
+        PRIMARY KEY (`preview_id`)
+        )
+*/
+
+@Table(name = "ep_widget_preview_files")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class EpWidgetPreviewFiles {
+       @Id
+       @GeneratedValue(strategy = GenerationType.AUTO)
+       @Column(name = "preview_id", length = 11, nullable = false, columnDefinition = "int(11) AUTO_INCREMENT")
+       @Digits(integer = 11, fraction = 0)
+       private Integer previewId;
+       @Column(name = "html_file", columnDefinition = "longblob DEFAULT NULL")
+       private byte[] htmlFile;
+       @Column(name = "css_file", columnDefinition = "longblob DEFAULT NULL")
+       private byte[] cssFile;
+       @Column(name = "javascript_file", columnDefinition = "longblob DEFAULT NULL")
+       private byte[] javascriptFile;
+       @Column(name = "framework_file", columnDefinition = "longblob DEFAULT NULL")
+       private byte[] frameworkFile;
+}
