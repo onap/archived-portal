20fe255c0be59e3d75ac85ac8e73faadb9037b48
Dominik Mizyn <d.mizyn@samsung.com>
2019-08-16 12:34:12 +0200

    Portal Spring Boot version Hibernate implementation
    Create classes that represent tables in the original database.
    
    Issue-ID: PORTAL-705
    Change-Id: I705d01cbd341e3627a7469d0d9f59885c8ff26fe
    Signed-off-by: Dominik Mizyn <d.mizyn@samsung.com>

---
+++ portal-BE/src/main/java/org/onap/portal/domain/db/fn/FnQzLocks.java
@@ -0,0 +1,99 @@
+/*
+ * ============LICENSE_START==========================================
+ * ONAP Portal
+ * ===================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ===================================================================
+ * Modifications Copyright (c) 2019 Samsung
+ * ===================================================================
+ *
+ * Unless otherwise specified, all software contained herein is licensed
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this software except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Unless otherwise specified, all documentation contained herein is licensed
+ * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
+ * you may not use this documentation except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             https://creativecommons.org/licenses/by/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, documentation
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ============LICENSE_END============================================
+ *
+ *
+ */
+
+package org.onap.portal.domain.db.fn;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.SafeHtml;
+import org.onap.portal.domain.db.fn.FnQzLocks.FnQzLocksID;
+
+/*
+CREATE TABLE `fn_qz_locks` (
+        `SCHED_NAME` varchar(120) NOT NULL,
+        `LOCK_NAME` varchar(40) NOT NULL,
+        PRIMARY KEY (`SCHED_NAME`,`LOCK_NAME`)
+        )
+*/
+
+@Table(name = "fn_qz_locks")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
+@Entity
+@IdClass(FnQzLocksID.class)
+public class FnQzLocks {
+       @Id
+       @Size(max = 120)
+       @SafeHtml
+       @Column(name = "SCHED_NAME", length = 120, nullable = false)
+       private String schedName;
+       @Id
+       @Size(max = 40)
+       @SafeHtml
+       @Column(name = "LOCK_NAME", length = 40, nullable = false)
+       private String lockName;
+
+       @Getter
+       @Setter
+       @NoArgsConstructor
+       @EqualsAndHashCode
+       @AllArgsConstructor
+       public static class FnQzLocksID implements Serializable {
+              @Size(max = 120)
+              @SafeHtml
+              private String schedName;
+              @Size(max = 40)
+              @SafeHtml
+              private String lockName;
+       }
+}
