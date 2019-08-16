6137cef448e7c52ad73bb20f3dad63a6fa37782c
Dominik Mizyn <d.mizyn@samsung.com>
2019-08-16 12:13:22 +0200

    Portal Spring Boot version Hibernate implementation
    Create classes that represent tables in the original database.
    
    Issue-ID: PORTAL-705
    Change-Id: Ic850ddf36c63be40644863bc69833aa2f76a5f9d
    Signed-off-by: Dominik Mizyn <d.mizyn@samsung.com>

---
+++ portal-BE/src/main/java/org/onap/portal/domain/db/fn/FnLuAlertMethod.java
@@ -0,0 +1,93 @@
+/*
+ * ============LICENSE_START==========================================
+ * ONAP Portal
+ * ===================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ===================================================================
+ * Modifications Copyright (c) 2019 Samsung
+ * ===================================================================
+ *
+ * Unless otherwise specified, all software contained herein is licensed
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this software except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Unless otherwise specified, all documentation contained herein is licensed
+ * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
+ * you may not use this documentation except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             https://creativecommons.org/licenses/by/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, documentation
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ============LICENSE_END============================================
+ *
+ *
+ */
+
+package org.onap.portal.domain.db.fn;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.SafeHtml;
+
+/*
+CREATE TABLE `fn_lu_alert_method` (
+        `alert_method_cd` varchar(10) NOT NULL,
+        `alert_method` varchar(50) NOT NULL,
+        PRIMARY KEY (`alert_method_cd`)
+        )
+*/
+
+@Table(name = "fn_lu_alert_method")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class FnLuAlertMethod {
+       @Id
+       @Column(name = "alert_method_cd", length = 10, nullable = false)
+       @Size(max = 50)
+       @SafeHtml
+       private String alertMethodCd;
+       @Column(name = "alert_method", length = 50, nullable = false)
+       @Size(max = 50)
+       @SafeHtml
+       @NotNull
+       private String alertMethod;
+
+       @OneToMany(
+               targetEntity = FnUser.class,
+               mappedBy = "alertMethodCd",
+               cascade = CascadeType.ALL,
+               fetch = FetchType.LAZY
+       )
+       private List<FnUser> fnUsers = new ArrayList<>();
+}
