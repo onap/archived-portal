dd26047ef91bc47d54ff1b7ecd5c3e36f5669bec
Dominik Mizyn <d.mizyn@samsung.com>
2019-08-16 10:19:41 +0200

    Portal Spring Boot version Hibernate implementation
    Create classes that represent tables in the original database.
    
    Issue-ID: PORTAL-705
    Change-Id: I0fddec3ec683286f3b807f342498ba4bfb4e7a8d
    Signed-off-by: Dominik Mizyn <d.mizyn@samsung.com>

---
+++ portal-BE/src/main/java/org/onap/portal/domain/db/cr/CrReportTemplateMap.java
@@ -0,0 +1,82 @@
+/*
+ * ============LICENSE_START==========================================
+ * ONAP Portal
+ * ===================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ===================================================================
+ * Modifications Copyright (c) 2019 Samsung
+ * ===================================================================
+ *
+ * Unless otherwise specified, all software contained herein is licensed
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this software except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Unless otherwise specified, all documentation contained herein is licensed
+ * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
+ * you may not use this documentation except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             https://creativecommons.org/licenses/by/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, documentation
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ============LICENSE_END============================================
+ *
+ *
+ */
+
+package org.onap.portal.domain.db.cr;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.SafeHtml;
+
+/*
+CREATE TABLE `cr_report_template_map` (
+        `report_id` int(11) NOT NULL,
+        `template_file` varchar(200) DEFAULT NULL,
+        PRIMARY KEY (`report_id`)
+        )
+*/
+
+@Table(name = "cr_report_template_map")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class CrReportTemplateMap {
+       @Id
+       @Column(name = "report_id", nullable = false)
+       @NotNull
+       private Long reportId;
+       @Column(name = "template_file", length = 200)
+       @Pattern(regexp = "[YNyn]")
+       @Size(max = 200)
+       @SafeHtml
+       private String templateFile;
+}
