aab9c41908e4a596a3143904b0d1fb77d5b49645
ezhil <ezhrajam@in.ibm.com>
2020-04-15 21:03:40 +0530

    Junit for SessionMgtRegistry
    Issue-ID: PORTAL-814
    Change-Id: I1aab4e26619a5fb24187d106df5e3932ad038bd6
    Signed-off-by: ezhil <ezhrajam@in.ibm.com>

---
+++ ecomp-portal-BE-os/src/test/java/org/onap/portalapp/scheduler/SessionMgtRegistryTest.java
@@ -0,0 +1,60 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP : Portal
+ * ================================================================================
+ * Copyright (C) 2020 IBM Intellectual Property. All rights reserved.
+ *=================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.portalapp.scheduler;
+
+import static org.junit.Assert.assertNotNull;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.onap.portalapp.service.sessionmgt.TimeoutHandler;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+
+public class SessionMgtRegistryTest {
+
+    @Mock
+    JobDetailFactoryBean job;
+    @Mock
+    TimeoutHandler lj;
+    @InjectMocks
+    SessionMgtRegistry lr;
+
+    String groupName = "AppGroup";
+    String jobName = "LogJob";
+    @Before
+    public void initialize(){
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void jobDetailFactoryBeantest() throws ParseException{
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("units", "bytes");
+        job.setJobClass(TimeoutHandler.class);
+        job.setJobDataAsMap(map);
+        job.setGroup(groupName);
+        job.setName(jobName);
+        assertNotNull(lr.jobDetailFactoryBean());
+    }
+}
