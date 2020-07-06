68e1bd7072ec1744a6fd219fe5e30cf6015f36e0
jz385p <jegadeesh.babu@att.com>
2020-07-06 13:19:43 +0530

    AuditLog code included to front end
    Audit log code added
    
    Issue-ID: PORTAL-941
    Change-Id: I59d0b2d4cf9d61ec7ecf304e99a5b3ed2154d4a7
    Signed-off-by: jz385p <jegadeesh.babu@att.com>

---
+++ portal-FE-common/src/app/shared/services/auditLog/audit-log.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+import { AuditLogService } from './audit-log.service';
+import { HttpClientTestingModule} from '@angular/common/http/testing';
+
+describe('AuditLogService', () => {
+  beforeEach(() => TestBed.configureTestingModule({imports: [HttpClientTestingModule]}));
+
+  it('should be created', () => {
+    const service: AuditLogService = TestBed.get(AuditLogService);
+    expect(service).toBeTruthy();
+  });
+});
---
+++ portal-FE-common/src/app/shared/services/auditLog/audit-log.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuditLogService {
+
+  constructor(private api: HttpClient) { }
+
+  storeAudit(affectedAppId:any,type:any,comment:any): Observable<any> {
+    var url = environment.api.storeAuditLog+'?affectedAppId=' + affectedAppId;
+        	if(type!=''){
+        		url= url+'&type='+type;
+        	}
+        	if(comment!=''){
+        		url= url+'&comment='+comment;
+        	}
+    return this.api.get(url);
+  }
+}
