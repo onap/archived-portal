977eb208cc5eec9ef249fa3f56f9c0f1d86ed4dc
Sri Balaji Marripud <sri.balaji.marripud@att.com>
2020-08-05 09:45:02 -0400

    Internationalization code for portal
    Issue-ID: PORTAL-975
    Change-Id: Id7411346e5a88cf596a422e14811bdfa178c403a
    Signed-off-by: Sri Balaji Marripud <sri.balaji.marripud@att.com>

--- portal-FE-common/src/app/layout/layout.component.spec.ts
+++ portal-FE-common/src/app/layout/layout.component.spec.ts
@@ -38,7 +38,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LayoutComponent } from './layout.component';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
