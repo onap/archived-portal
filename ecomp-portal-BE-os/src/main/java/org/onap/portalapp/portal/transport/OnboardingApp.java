05afd9ca4eb98b49f3751b30238128ed1929e441
Sudarshan Kumar <sudarshan.kumar@att.com>
2020-08-26 17:35:43 +0530

    Fixed App Onboarding - List page error
    App Onboarding - List page throwing error while loading Apps
    
    Issue-ID: PORTAL-994
    Change-Id: Ic1eaf0fd41b5059eb46fdbb35626a558fb53b956
    Signed-off-by: Sudarshan Kumar <sudarshan.kumar@att.com>

--- ecomp-portal-BE-os/src/main/java/org/onap/portalapp/portal/transport/OnboardingApp.java
+++ ecomp-portal-BE-os/src/main/java/org/onap/portalapp/portal/transport/OnboardingApp.java
@@ -31,7 +31,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * ============LICENSE_END============================================
+ * ============LICENSE_END===========================================
  *
  * 
  */
