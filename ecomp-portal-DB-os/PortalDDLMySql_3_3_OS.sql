28c5792ebdceca11b604609dc52a42db2830778f
Sudarshan Kumar <sudarshan.kumar@att.com>
2020-08-28 15:08:22 +0530

    Updated docker image DB script version to 3_3
    Updated docker image DB script version to 3_3
    
    Issue-ID: PORTAL-995
    Change-Id: I5e4b27ac2e30e936d860aed484843d5676a35012
    Signed-off-by: Sudarshan Kumar <sudarshan.kumar@att.com>

---
+++ ecomp-portal-DB-os/PortalDDLMySql_3_3_OS.sql
@@ -0,0 +1,12 @@
+-- ---------------------------------------------------------------------------------------------------------------
+-- This script adds tables for the OPEN-SOURCE 2.1.0 version of the Portal database.
+-- The COMMON DDL script must be executed first!
+-- ---------------------------------------------------------------------------------------------------------------
+
+SET FOREIGN_KEY_CHECKS=1; 
+
+USE portal;
+
+-- No additional tables required at this time
+
+commit;
